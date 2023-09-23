508dcba9062343f68ea8eee15bf4e5c4611dccaf
Jordan <penttijordo@gmail.com>
2023-09-23 15:32:57 -0400

    actual push

---
+++ app/api/server_routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import Server
+
+server_routes = Blueprint('servers', __name__)
+
+@server_routes.route('/')
+@login_required
+def all_public_servers():
+    """
+    Get all public servers.
+    """
+    public_servers = Server.query.filter(Server.private.is_boolean(False)).all()
+    return {'servers': [server.to_dict() for server in public_servers]}
