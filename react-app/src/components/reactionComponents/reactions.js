ce0ade391e0b05fbc3401a633c5e06fc660ce074
Jordan <penttijordo@gmail.com>
2023-09-26 16:41:53 -0400

    got reaction picker to render

--- react-app/src/components/reactionComponents/reactions.js
+++ react-app/src/components/reactionComponents/reactions.js
@@ -1,15 +1,37 @@
-import EmojiPicker, { Emoji, EmojiStyle } from 'emoji-picker-react'
+import EmojiPicker, { Emoji, EmojiStyle, EmojiClickData } from 'emoji-picker-react'
+import { useState } from 'react';
+
 
 function Reactions() {
+    const [selectedEmoji, setSelectedEmoji] = useState("1f60a");
+    const [inputValue, setInputValue] = useState("");
+
+
+
+    function onClick(EmojiClickData, MouseEvent) {
+        let emojiData = EmojiClickData
+        console.log("EmojiClickData.unified", EmojiClickData.unified)
+        setInputValue(
+          (inputValue) =>
+            inputValue + (emojiData.isCustom ? emojiData.unified : emojiData.emoji)
+        );
+        setSelectedEmoji(EmojiClickData.unified);
+
+      }
     return (
       <div>
-        <EmojiPicker />
+        <EmojiPicker
+            onEmojiClick={onClick}
+            autoFocusSearch={false}
+            emojiStyle={EmojiStyle.DARK}
+            theme={'dark'}
+        />
         <p>
             My Favorite emoji is:
-            <Emoji unified="1f123" size="25" />
+            <Emoji unified={selectedEmoji} size="25" />
         </p>
       </div>
     );
-  }
+}
 
 export default Reactions
