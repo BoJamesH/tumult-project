d1b69a7ab7f1558b270d6e9f4034a76705fba7cf
Bo James Hancock <RobertJamesH@gmail.com>
2023-09-27 12:36:02 -0700

    Renaming folders

--- react-app/src/components/loginFormModal/index.js
+++ react-app/src/components/loginFormModal/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { login } from "../../store/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
-import "./LoginForm.css";
+import './LoginForm.css'
 import { useHistory } from "react-router-dom"
 
 
