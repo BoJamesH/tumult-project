443df64cc682f5d481dc122f22fc28b30b62481e
Ryan <rjrose.19@gmail.com>
2023-09-27 13:30:38 -0400

    Troubleshooting websockets

--- app/socketio.py
+++ app/socketio.py
@@ -1,10 +1,11 @@
 from flask_socketio import SocketIO
+from app.models import Message, db
 import os
 
 if os.environ.get("FLASK_ENV") == "production":
     origins = [
-        "http://actual-app-url.herokuapp.com",
-        "https://actual-app-url.herokuapp.com"
+        "http://tumult-websocket.onrender.com",
+        "https://tumult-websocket.onrender.com"
     ]
 else:
     origins = "*"
@@ -14,5 +15,14 @@ socketio = SocketIO(cors_allowed_origins=origins)
 
 @socketio.on("chat")
 def handle_chat(data):
+    if data['message_text'] != "has connected!" or data['message_text'] != 'has disconnected!':
+        new_chat = Message(
+            message_text = data['message_text'],
+            user_id = data['user_id'],
+            server_id = data['server_id'],
+            channel_id = data['channel_id']
+        )
+    db.session.add(new_chat)
+    db.session.commit()
     emit("chat", data, broadcast=True)
     # code to follow
