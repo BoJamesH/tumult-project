d23d8c2e50c1d59dbd81be25f5c44c61d53b3070
Bo James Hancock <RobertJamesH@gmail.com>
2023-09-28 08:34:00 -0700

    Minor pregrade prep

--- react-app/src/components/publicServers/publicServers.js
+++ react-app/src/components/publicServers/publicServers.js
@@ -2,8 +2,10 @@ import { useDispatch, useSelector } from 'react-redux'
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { getPublicServers  } from '../../store/servers'
+import { useHistory } from 'react-router-dom'
 
 const PublicServers = () => {
+    const history = useHistory()
     const dispatch = useDispatch()
     const servers = useSelector( state => state.servers.allServers )
 
@@ -13,17 +15,27 @@ const PublicServers = () => {
 
     console.log("SERVER DATA", servers)
 
+    const handleNewServer = (e) => {
+        e.preventDefault()
+        history.push('/servers/new')
+    }
+
     if(!servers.length) return null
 
     return(
-        <div className='public-server-list'>
-            {servers.map(server => {
-                if (!server.id) return null
-                return (
-                    <div key={server.id} className='server-card'><Link to={`/servers/${server.id}`}>{server.name}</Link></div>
-                )
-            })}
-        </div>
+        <>
+            <div className='public-server-list'>
+                {servers.map(server => {
+                    if (!server.id) return null
+                    return (
+                        <div key={server.id} className='server-card'><Link to={`/servers/${server.id}`}>{server.name}</Link></div>
+                    )
+                })}
+            </div>
+            <div>
+                <button to='/servers/new' onClick={handleNewServer}>Create a new server</button>
+            </div>
+        </>
     )
 }
 
