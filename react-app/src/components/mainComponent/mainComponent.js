691ad51566d24762532a91fb52e64327ca47c642
Bo James Hancock <RobertJamesH@gmail.com>
2023-09-28 15:24:37 -0700

    Hopefully finished main component

--- react-app/src/components/mainComponent/mainComponent.js
+++ react-app/src/components/mainComponent/mainComponent.js
@@ -7,18 +7,20 @@ import { useEffect, useState } from "react"
 
 const Main = () => {
 
-    const {serverId} = useParams()
-    const {channelId} = useParams()
-    const [serverFocusId, setServerFocusId] = useState()
-    const [channelFocusId, setChannelFocusId] = useState()
+    // const {serverId} = useParams()
+    // const {channelId} = useParams()
+    // const [serverFocusId, setServerFocusId] = useState()
+    // const [channelFocusId, setChannelFocusId] = useState()
 
-    console.log('-----------------------')
-    console.log('Server ID: ',serverId)
-    console.log('-----------------------')
+    // console.log('-----------------------')
+    // console.log('Server ID: ',serverId)
+    // console.log('-----------------------')
 
-    useEffect( async () => {
-        setServerFocusId(serverId)
-    },[serverId])
+    // useEffect( async () => {
+    //     setServerFocusId(serverId)
+    // },[serverId])
+
+    
 
     return (
         <>
