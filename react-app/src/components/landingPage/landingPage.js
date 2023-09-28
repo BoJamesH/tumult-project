b0a2f016e355a4ce883357bf86c2ce11d4c7d712
Ryan <rjrose.19@gmail.com>
2023-09-28 14:11:15 -0400

    still working landing page

--- react-app/src/components/landingPage/landingPage.js
+++ react-app/src/components/landingPage/landingPage.js
@@ -15,27 +15,50 @@ function LandingPage({user}){
     return (
         <>
             <nav>
-                <div>
+                <div className="home-nav-container">
                     <img className="discordLogo" src="../images/icon_clyde_black_RGB.png" alt="Tumult"/>
-                </div>
+                    <span>
+                        Fill
+                    </span>
+                    <span>
+                        Fill2
+                    </span>
+                    <span>
+                        Fill3
+                    </span>
+                    <span>
+                        Fill4
+                    </span>
                 { user ? (
-                  <button onClick={handleOpenDiscord}>Open Discord</button>
+                    <button onClick={handleOpenDiscord}>Open Discord</button>
                     ) : (
                         <OpenModalButton
                         buttonText="Log In"
-                        //   onItemClick={closeMenu}
                         modalComponent={<LoginFormModal />}
                         />
                     )
                 }
+                </div>
             </nav>
+            <div className="header-image-container">
+            <img className="leftheaderimage" src="../images/headerleft_splashart.png" alt="Tumult"/>
+            <div className="imagine-container">
+                <div >
+                    <h1>IMAGINE A PLACE...</h1>
+                    <div>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</div>
+                </div>
+                <div>
+                    <button>Log In</button>
+                    <button>Sign Up</button>
+                </div>
+            </div>
+            <img className="rightheaderimage" src="../images/headerright_splashart.png" alt="Tumult"/>
+            </div>
         <div>
             <div>
             </div>
-            <img src="../images/header_blurpleclouds.png" alt="Tumult"/>
+            <img className="blueClouds" src="../images/header_blurpleclouds.png" alt="Tumult"/>
             <img src="../images/header_redclouds.png" alt="Tumult"/>
-            <img src="../images/headerleft_splashart.png" alt="Tumult"/>
-            <img src="../images/headerright_splashart.png" alt="Tumult"/>
             <img src="../images/pagebottom_splashart.png" alt="Tumult"/>
             <img src="../images/pagelower_splashart.png" alt="Tumult"/>
             <img src="../images/pagemiddle_splashart.png" alt="Tumult"/>
