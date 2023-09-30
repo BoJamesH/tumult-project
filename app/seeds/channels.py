0ef2049087b5f9e90745b6c4da4ca245b37948db
Bo James Hancock <RobertJamesH@gmail.com>
2023-09-30 16:18:38 -0700

    Added server and channel seed data

--- app/seeds/channels.py
+++ app/seeds/channels.py
@@ -4,22 +4,145 @@ from sqlalchemy.sql import text
 
 # Adds a demo user, you can add other users here if you want
 def seed_channels():
-    channel1 = Channel(
-        name='channel1', owner_id=1, server_id=1, private=False)
+  channel1 = Channel(
+        name='general',
+        owner_id=1,
+        server_id=1,
+        private=False
+    )
+
     channel2 = Channel(
-        name='channel1', owner_id=2, server_id=2, private=False)
+        name='general',
+        owner_id=2,
+        server_id=2,
+        private=False
+    )
+
     channel3 = Channel(
-        name='channel1', owner_id=3, server_id=3, private=False)
+        name='general',
+        owner_id=3,
+        server_id=3,
+        private=False
+    )
+
     channel4 = Channel(
-        name='channel1', owner_id=4, server_id=4, private=False)
+        name='general',
+        owner_id=4,
+        server_id=4,
+        private=False
+    )
+
     channel5 = Channel(
-        name='channel1', owner_id=5, server_id=5, private=False)
+        name='general',
+        owner_id=5,
+        server_id=5,
+        private=False
+    )
+
     channel6 = Channel(
-        name='channel1', owner_id=4, server_id=6, private=False)
+        name='general',
+        owner_id=1,
+        server_id=6,
+        private=False
+    )
+
     channel7 = Channel(
-        name='channel2', owner_id=1, server_id=1, private=False)
+        name='general',
+        owner_id=2,
+        server_id=7,
+        private=False
+    )
+
     channel8 = Channel(
-        name='channel2', owner_id=2, server_id=2, private=False)
+        name='general',
+        owner_id=3,
+        server_id=8,
+        private=False
+    )
+
+    channel9 = Channel(
+        name='general',
+        owner_id=4,
+        server_id=9,
+        private=False
+    )
+
+    channel10 = Channel(
+        name='general',
+        owner_id=5,
+        server_id=10,
+        private=False
+    )
+
+    channel11 = Channel(
+        name='general',
+        owner_id=1,
+        server_id=11,
+        private=False
+    )
+
+    channel12 = Channel(
+        name='general',
+        owner_id=2,
+        server_id=12,
+        private=False
+    )
+
+    channel13 = Channel(
+        name='general',
+        owner_id=3,
+        server_id=13,
+        private=False
+    )
+
+    channel14 = Channel(
+        name='general',
+        owner_id=4,
+        server_id=14,
+        private=False
+    )
+
+    channel15 = Channel(
+        name='general',
+        owner_id=5,
+        server_id=15,
+        private=False
+    )
+
+    channel16 = Channel(
+        name='general',
+        owner_id=1,
+        server_id=16,
+        private=False
+    )
+
+    channel17 = Channel(
+        name='general',
+        owner_id=2,
+        server_id=17,
+        private=False
+    )
+
+    channel18 = Channel(
+        name='general',
+        owner_id=3,
+        server_id=18,
+        private=False
+    )
+
+    channel19 = Channel(
+        name='general',
+        owner_id=4,
+        server_id=19,
+        private=False
+    )
+
+    channel20 = Channel(
+        name='general',
+        owner_id=5,
+        server_id=20,
+        private=False
+    )
 
 
     db.session.add(channel1)
@@ -30,6 +153,18 @@ def seed_channels():
     db.session.add(channel6)
     db.session.add(channel7)
     db.session.add(channel8)
+    db.session.add(channel9)
+    db.session.add(channel10)
+    db.session.add(channel11)
+    db.session.add(channel12)
+    db.session.add(channel13)
+    db.session.add(channel14)
+    db.session.add(channel15)
+    db.session.add(channel16)
+    db.session.add(channel17)
+    db.session.add(channel18)
+    db.session.add(channel19)
+    db.session.add(channel20)
     db.session.commit()
 
 
--- app/seeds/servers.py
+++ app/seeds/servers.py
@@ -5,18 +5,144 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_servers():
     server1 = Server(
-        name='Server1', owner_id=1, label_image='https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', private=False)
-    server2 = Server(
-        name='Server2', owner_id=2, label_image='https://images.pexels.com/photos/3777200/pexels-photo-3777200.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', private=False)
-    server3 = Server(
-        name='Server3', owner_id=3, label_image='https://spaceplace.nasa.gov/gallery-sun/en/solar-flare.en.jpg', private=False)
-    server4 = Server(
-        name='Server4', owner_id=4, label_image='https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg', private=False)
-    server5 = Server(
-        name='Server5', owner_id=5, label_image='https://media.istockphoto.com/id/636379014/photo/hands-forming-a-heart-shape-with-sunset-silhouette.jpg?s=612x612&w=0&k=20&c=CgjWWGEasjgwia2VT7ufXa10azba2HXmUDe96wZG8F0=', private=False)
-    server6 = Server(
-        name='Server6', owner_id=4, label_image='https://img.freepik.com/premium-photo/planet-earth-illustration_691833-247.jpg?w=360', private=False)
+        name='Server1',
+        owner_id=1,
+        label_image='https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        private=False
+    )
 
+    server2 = Server(
+        name='Server2',
+        owner_id=2,
+        label_image='https://images.pexels.com/photos/3777200/pexels-photo-3777200.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        private=False
+    )
+
+    server3 = Server(
+        name='Server3',
+        owner_id=3,
+        label_image='https://spaceplace.nasa.gov/gallery-sun/en/solar-flare.en.jpg',
+        private=False
+    )
+
+    server4 = Server(
+        name='Server4',
+        owner_id=4,
+        label_image='https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg',
+        private=False
+    )
+
+    server5 = Server(
+        name='Server5',
+        owner_id=5,
+        label_image='https://media.istockphoto.com/id/636379014/photo/hands-forming-a-heart-shape-with-sunset-silhouette.jpg?s=612x612&w=0&k=20&c=CgjWWGEasjgwia2VT7ufXa10azba2HXmUDe96wZG8F0=',
+        private=False
+    )
+
+    server6 = Server(
+        name='Server6',
+        owner_id=6,
+        label_image='https://img.freepik.com/premium-photo/planet-earth-illustration_691833-247.jpg?w=360',
+        private=False
+    )
+
+    server7 = Server(
+        name='Server7',
+        owner_id=1,
+        label_image='https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        private=False
+    )
+
+    server8 = Server(
+        name='Server8',
+        owner_id=2,
+        label_image='https://images.pexels.com/photos/3777200/pexels-photo-3777200.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        private=False
+    )
+
+    server9 = Server(
+        name='Server9',
+        owner_id=3,
+        label_image='https://spaceplace.nasa.gov/gallery-sun/en/solar-flare.en.jpg',
+        private=False
+    )
+
+    server10 = Server(
+        name='Server10',
+        owner_id=4,
+        label_image='https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg',
+        private=False
+    )
+
+    server11 = Server(
+        name='Server11',
+        owner_id=5,
+        label_image='https://media.istockphoto.com/id/636379014/photo/hands-forming-a-heart-shape-with-sunset-silhouette.jpg?s=612x612&w=0&k=20&c=CgjWWGEasjgwia2VT7ufXa10azba2HXmUDe96wZG8F0=',
+        private=False
+    )
+
+    server12 = Server(
+        name='Server12',
+        owner_id=6,
+        label_image='https://img.freepik.com/premium-photo/planet-earth-illustration_691833-247.jpg?w=360',
+        private=False
+    )
+
+    server13 = Server(
+        name='Server13',
+        owner_id=1,
+        label_image='https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        private=False
+    )
+
+    server14 = Server(
+        name='Server14',
+        owner_id=2,
+        label_image='https://images.pexels.com/photos/3777200/pexels-photo-3777200.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        private=False
+    )
+
+    server15 = Server(
+        name='Server15',
+        owner_id=3,
+        label_image='https://spaceplace.nasa.gov/gallery-sun/en/solar-flare.en.jpg',
+        private=False
+    )
+
+    server16 = Server(
+        name='Server16',
+        owner_id=4,
+        label_image='https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg',
+        private=False
+    )
+
+    server17 = Server(
+        name='Server17',
+        owner_id=5,
+        label_image='https://media.istockphoto.com/id/636379014/photo/hands-forming-a-heart-shape-with-sunset-silhouette.jpg?s=612x612&w=0&k=20&c=CgjWWGEasjgwia2VT7ufXa10azba2HXmUDe96wZG8F0=',
+        private=False
+    )
+
+    server18 = Server(
+        name='Server18',
+        owner_id=6,
+        label_image='https://img.freepik.com/premium-photo/planet-earth-illustration_691833-247.jpg?w=360',
+        private=False
+    )
+
+    server19 = Server(
+        name='Server19',
+        owner_id=1,
+        label_image='https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        private=False
+    )
+
+    server20 = Server(
+        name='Server20',
+        owner_id=2,
+        label_image='https://images.pexels.com/photos/3777200/pexels-photo-3777200.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        private=False
+    )
 
     db.session.add(server1)
     db.session.add(server2)
@@ -24,6 +150,20 @@ def seed_servers():
     db.session.add(server4)
     db.session.add(server5)
     db.session.add(server6)
+    db.session.add(server7)
+    db.session.add(server8)
+    db.session.add(server9)
+    db.session.add(server10)
+    db.session.add(server11)
+    db.session.add(server12)
+    db.session.add(server13)
+    db.session.add(server14)
+    db.session.add(server15)
+    db.session.add(server16)
+    db.session.add(server17)
+    db.session.add(server18)
+    db.session.add(server19)
+    db.session.add(server20)
     db.session.commit()
 
 
